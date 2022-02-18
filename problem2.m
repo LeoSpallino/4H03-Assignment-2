b5f4c06d1072b79205ad87ad5350ff9b3393ea4d
Leo Spallino <spallino@icloud.com>
2022-02-18 00:30:44 -0500

    Create plot for part 2

--- problem2.m
+++ problem2.m
@@ -24,3 +24,21 @@ T(:, [1,2,7,8,9,10,11,12,13,14]) = [];
 % Building the PCA models
 % ----------------------------------------------------------------------- %
 [t, p, R2] = nipalspca(T,3);
+
+% Part 2 - Scores plot
+% ----------------------------------------------------------------------- %
+point = 250;
+low_t = floor(length(t)/2);
+scoreplot(t(:, 1), t(:, 2));
+hold on;
+plot(t(1:low_t,1),t(1:low_t,2),'ro','MarkerFaceColor','r');
+plot(t(low_t+1:length(t),1),t(low_t+1:length(t),2),'bo','MarkerFaceColor','b');
+hold off;
+
+
+
+
+
+
+
+
